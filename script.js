ffe193b78f17254b0d7a1f9f1b38bc0bea6ceac2
TFmane <tfmanemail@gmail.com>
2017-05-17 10:39:08 +0200

    main site
    this is the code for the main site and style

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("myBtn").style.display = "block";
+	} else {
+		document.getElementById("myBtn").style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+$(function(){
+  $(".namenu a").click(function(e){
+    e.preventDefault();
+    $('html,body').scrollTo(this.hash,this.hash); 
+  });
+});
